86e26f57c35982304dc96afe3a7bea35ffd3533a
divam002 <divamaggarwal21@gmail.com>
2022-04-21 20:59:03 +0530

    firebase

--- config.js
+++ config.js
@@ -2,7 +2,12 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
+    apiKey: "AIzaSyDDcKvidQ_IGNe1DTf6c0zd8DTfnfdMzMw",
+    authDomain: "project-67-f30f6.firebaseapp.com",
+    projectId: "project-67-f30f6",
+    storageBucket: "project-67-f30f6.appspot.com",
+    messagingSenderId: "899167275012",
+    appId: "1:899167275012:web:fb459d4033af7ef0cc0d51"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
